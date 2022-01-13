a65c184e0093a1aeb58b7bb4b8235d8f3a724049
Gao-Hongxiang <gaohongxiang_i@didiglobal.com>
2022-01-13 14:41:01 +0800

    init路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+// 导入路由
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+
+// 导入页面组件
+import Login from './pages/Login';
+import Layout from './pages/Layout';
+
 import './App.scss';
 
 function App() {
-  return <div className="app">App</div>;
+  return (
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route exact path="/" render={() => <Redirect to="/home" />} />;
+          <Route path="/home" component={Layout} />
+          <Route path="/login" component={Login} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
