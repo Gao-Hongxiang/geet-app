9c41b134ec6804c46d36f15be5d321fdc77e4120
Gao-Hongxiang <gaohongxiang_i@didiglobal.com>
2022-01-20 20:07:18 +0800

    布局模板

--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -1,4 +1,55 @@
+import { Route } from "react-router-dom"
+import { TabBar } from "antd-mobile"
+import styles from "./index.module.scss"
+
+import Icon from "@/components/Icon"
+
+// 导入页面组件，配置路由
+import Home from "@/pages/Home"
+import Question from "@/pages/Question"
+import Video from "@/pages/Video"
+import Profile from "@/pages/Profile"
+
+const tabs = [
+  { path: "/home/index", icon: "iconbtn_home", text: "首页" },
+  { path: "/home/question", icon: "iconbtn_qa", text: "问答" },
+  { path: "/home/video", icon: "iconbtn_video", text: "视频" },
+  { path: "/home/profile", icon: "iconbtn_mine", text: "我的" },
+]
+
 const Layout = () => {
-  return <div>首页</div>;
-};
-export default Layout;
+  return (
+    <div className={styles.root}>
+      {/* 二级嵌套路由 */}
+      <Route exact path="/home/index">
+        <Home></Home>
+      </Route>
+      <Route path="/home/question">
+        <Question></Question>
+      </Route>
+      <Route path="/home/video">
+        <Video></Video>
+      </Route>
+      <Route path="/home/profile">
+        <Profile></Profile>
+      </Route>
+
+      <TabBar className="tab-bar">
+        {tabs.map((item) => (
+          <TabBar.Item
+            key={item.path}
+            icon={(active: boolean) => (
+              <Icon
+                type={active ? `${item.icon}_sel` : item.icon}
+                className="tab-bar-item-icon"
+              />
+            )}
+            title={item.text}
+          />
+        ))}
+      </TabBar>
+    </div>
+  )
+}
+
+export default Layout
